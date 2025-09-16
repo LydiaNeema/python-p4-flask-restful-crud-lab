2a145c37ad40707168db9896070dea4c76064159
Lydia Neema <lydianeema25@gmail.com>
2025-09-16 11:11:31 +0300

    updated test

--- server/app.py
+++ server/app.py
@@ -16,7 +16,9 @@ db.init_app(app)
 
 api = Api(app)
 
-
+# --------------------------
+# /plants
+# --------------------------
 class Plants(Resource):
 
     def get(self):
@@ -30,6 +32,7 @@ class Plants(Resource):
             name=data['name'],
             image=data['image'],
             price=data['price'],
+            is_in_stock=data.get('is_in_stock', True)  # default True if not provided
         )
 
         db.session.add(new_plant)
@@ -41,15 +44,45 @@ class Plants(Resource):
 api.add_resource(Plants, '/plants')
 
 
+# --------------------------
+# /plants/<id>
+# --------------------------
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        return make_response(plant.to_dict(), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
+# --------------------------
+# Run the app
+# --------------------------
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
